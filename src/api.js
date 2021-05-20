d6c997ab11badeb7dc788dd61399161b53b4f3f0
Hakan Demiral <hakan.98@outlook.com>
2021-05-20 04:32:19 +0300

    :)

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.apiBase|| "http://hakan.local:3001",
+    baseURL: "https://efdb.hakandemiral.com.tr",
 });
 
 export default api;
